3e68badd731cf6ef6a49e0491c08dbfb583f88a8
sermajid <sermajid3@yahoo.com>
2022-08-18 01:03:30 +0430

    remote test

--- 1.Two Sum/Solution.php
+++ 1.Two Sum/Solution.php
@@ -6,7 +6,6 @@ class Solution {
         $res=[];
         for ($i=0;$i<count($nums);$i++){
             $diff=$target-$nums[$i];
-
             if(in_array($diff, $setNums)){
                 $key=array_search($diff, $setNums);
                 $res[]=$i;
@@ -17,7 +16,3 @@ class Solution {
         return $res;
     }
 }
-
-$obj=new Solution();
-//print_r(array_search(3,[3,2,3,5]));
-print_r($obj->twoSum([2,7,11,15],9));
